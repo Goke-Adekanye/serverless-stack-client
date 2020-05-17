fb85947ddcb1e2c013a87723335c84766f9dc7a0
Goke-Adekanye <adekanyeadegoke96@gmail.com>
2020-05-17 13:56:40 +0100

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-bucketss"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ferv1monr1.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_PYIDXKzK1",
+      APP_CLIENT_ID: "1hhggjp40kf66vm3eluei86mgt",
+      IDENTITY_POOL_ID: "us-east-1:5b3025a3-8f69-4256-afae-535b480e38e5"
+    }
+  };
